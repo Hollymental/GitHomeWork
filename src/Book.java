ac1f5efd2f8bed974c483f42d18b2a09c3f5d3cf
Vlad Pishchugin <vlados.228.101@gmail.com>
2025-03-13 13:50:37 +0300

    AddBookReserve

--- src/Book.java
+++ src/Book.java
@@ -15,4 +15,12 @@ public class Book {
     public boolean isAvailable() {
         return isAvailable; // Проверяет доступность книги
     }
+    public void reserveBook() {
+        if (isAvailable) {
+            isAvailable = false; // Резервирует книгу
+            System.out.println("Книга \"" + title + "\" зарезервирована.");
+        } else {
+            System.out.println("Книга \"" + title + "\" уже зарезервирована.");
+        }
+    }
 }
