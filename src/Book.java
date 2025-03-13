25a47363acfdb82695c6af4025cbe87d25bf1808
Vlad Pishchugin <vlados.228.101@gmail.com>
2025-03-13 13:49:35 +0300

    AddBook

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true; // Книга доступна по умолчанию
+
+    }
+}
