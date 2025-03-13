359d74c8b6be86e129c7d362811765cfaaf7f766
Vlad Pishchugin <vlados.228.101@gmail.com>
2025-03-13 13:47:23 +0300

    AddCar

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    private String model;
+    private String brand;
+    private int year;
+
+    public Car(String model, String brand, int year) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+
+    }
+}
